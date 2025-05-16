c8a04ffcd427d3337a2deea0c42cd3bc553de74d
Humbert Dominguez <humbertodelac13@gmail.com>
2025-05-15 20:13:46 -0600

    conexion con websocket lista

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
